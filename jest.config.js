6d3ee4ce326a34f8296cc319e2db9710c5a8d1cb
pether.sorling@gmail.com <pether.sorling@gmail.com>
2020-11-20 20:01:15 +0100

    fix test coverage sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,26 @@
 module.exports = {
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
+  testEnvironment: 'node',
+  modulePaths: ['<rootDir>/src', 'node_modules'],
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-}
+  
+  coverageReporters: ['json', 'lcov', 'text'],
+  coveragePathIgnorePatterns: ['.*/src/.*\\.d\\.ts', '.*/src/testUtil/.*'],
+  testResultsProcessor: 'jest-sonar-reporter'
+
+  // // Use the below to set coverate goals
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 80,
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80
+  //   }
+  // }
+};
